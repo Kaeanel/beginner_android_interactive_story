a3b6475a6bd9c97f20b89e2dd0eaac72809c0295
Denis <denisfilipov.12@gmail.com>
2020-05-12 22:53:15 +0200

    added a new activity+class so we can begin our story

--- app/src/main/java/com/kaeanel/interactivestory/MainActivity.java
+++ app/src/main/java/com/kaeanel/interactivestory/MainActivity.java
@@ -2,6 +2,7 @@ package com.kaeanel.interactivestory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -25,8 +26,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String name = nameField.getText().toString();
-                Toast.makeText(MainActivity.this, name, Toast.LENGTH_LONG).show();
+                startStory();
             }
         });
+    }
+
+    private void startStory() {
+        Intent intent = new Intent(this, StoryActivity.class);
+        startActivity(intent);
     }
 }
