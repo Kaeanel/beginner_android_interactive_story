faf61a58f5804c58a855c785fc48faac9988fd0f
Denis <denisfilipov.12@gmail.com>
2020-05-14 22:37:34 +0200

    recovering the name from the StoryActivity

--- app/src/main/java/com/kaeanel/interactivestory/StoryActivity.java
+++ app/src/main/java/com/kaeanel/interactivestory/StoryActivity.java
@@ -2,13 +2,22 @@ package com.kaeanel.interactivestory;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StoryActivity extends AppCompatActivity {
+
+    public static final String TAG = StoryActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story);
+
+        Intent intent = getIntent();
+
+        String name = intent.getStringExtra("name");
+        Log.d(TAG,name);
     }
 }
